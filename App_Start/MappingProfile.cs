e209077cf7f85508a3a86de14f0235737ada548a
balint974 <balint97@yahoo.com>
2021-08-06 15:45:22 +0300

    Update Restful services

---
+++ App_Start/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly_New.DTOs;
+using Vidly_New.Models;
+
+namespace Vidly_New.App_Start
+{
+    public class MappingProfile:Profile
+    {
+        public MappingProfile()
+        {
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<CustomerDto, Customer>();
+        }
+    }
+}
